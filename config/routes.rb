ce6e6d4c7610eaa5e2d93d809e4ff018e4ccdb04
Adam Hammer <adam.hammer@gmail.com>
2011-06-19 00:25:33 -0400

    Finished CSS updates and added User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
     root :to => 'pages#home'
     match '/contact', :to => 'pages#contact'
     match '/help', :to => 'pages#help'
     match '/about', :to => 'pages#about'
+    match '/signup', :to => 'users#new'
     
 end
