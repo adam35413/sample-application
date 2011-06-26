e6f1ccd743124da4213c881e522ad4e3f24cded8
Adam Hammer <adam.hammer@gmail.com>
2011-06-26 13:56:59 -0400

    Done with user edit/update, index, etc.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,29 @@ module SessionsHelper
         @current_user ||= user_from_remeber_token
     end
 
+    def current_user?(user)
+        current_user == user   
+    end
+    
+    def deny_access
+      store_location
+      redirect_to signin_path, :notice => "Please sign in to access this page."
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      clear_return_to
+    end
+
     private
+
+        def store_location
+          session[:return_to] = request.fullpath
+        end
+
+        def clear_return_to
+          session[:return_to] = nil
+        end
 
         def user_from_remeber_token
             User.authenticate_with_salt(*remember_token)
