6cc339cac65479cc34e005417057ad2ae2782405
Adam Hammer <adam.hammer@gmail.com>
2011-07-04 19:32:44 -0400

    Transfer commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@
 class User < ActiveRecord::Base
     # Database connections
     has_many :microposts, :dependent => :destroy
+    has_many :relationships, :foreign_key => "follower_id",
+                             :dependent => :destroy
+    has_many :following, :through => :relationships, :source => :followed
 
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -50,6 +53,18 @@ class User < ActiveRecord::Base
 
     def feed
       Micropost.where("user_id = ?", id)
+    end
+
+    def following?(followed)
+      relationships.find_by_followed_id(followed)
+    end
+  
+    def follow!(followed)
+      relationships.create!(:followed_id => followed.id)
+    end
+    
+    def unfollow!(followed)
+      relationships.find_by_followed_id(followed).destroy
     end
 
     private
